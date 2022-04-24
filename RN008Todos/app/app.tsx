cceb4200ad2302fbac6bdfd8324c17ca36f107ec
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-04-24 23:03:54 +0700

    1

--- RN008Todos/app/app.tsx
+++ RN008Todos/app/app.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from './redux/store';
-import {TodoListScreen} from './screens/todo-list/todo-list.screen';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {AppStackNavigator} from './navigators/app-stack.navigator';
 const App = () => {
   return (
     <Provider store={store}>
-      <TodoListScreen />
+      <NavigationContainer>
+        <AppStackNavigator />
+      </NavigationContainer>
     </Provider>
   );
 };
